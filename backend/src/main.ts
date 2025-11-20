d20bdd4c78c4f7c65f2483adfc3e6e728f0ec52a
MedmakNP <95432399+MedmakNP@users.noreply.github.com>
2025-11-20 12:12:34 +0200

    Enable CORS for localhost frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3001'], // порт фронта
-    credentials: true,
+    origin: [
+      'http://localhost:3000', // твій React dev-сервер
+      'http://localhost:5173',
+      'https://clothshop-backend.onrender.com', // можна, але не обов’язково
+      // сюди потім додамо домен фронта, коли винесемо його на прод
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: false, // поки без cookies
   });
-  await app.listen(3000);
+
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
