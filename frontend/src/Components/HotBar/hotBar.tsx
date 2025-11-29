a5ccbd944d0153666b57b30cf36b82bd9d82e2cd
MedmakNP <95432399+MedmakNP@users.noreply.github.com>
2025-11-29 14:45:20 +0200

    fix admin№4

--- frontend/src/Components/HotBar/hotBar.tsx
+++ frontend/src/Components/HotBar/hotBar.tsx
@@ -11,19 +11,22 @@ function HotBar() {
   return (
     <nav className={`${styles.hotBar} ${styles[isBlurred]}`}>
       <div className={styles.container}>
-        <Link to="/TechWorld-TypeScript" className={`${styles.link} ${styles[type]}`}>
+        <Link to="/" className={`${styles.link} ${styles[type]}`}>
           {t("hotBar.home")}
         </Link>
-        <Link to="/TechWorld-TypeScript/shopPage" className={`${styles.link} ${styles[type]}`}>
+        <Link to="/shopPage" className={`${styles.link} ${styles[type]}`}>
           {t("hotBar.shop")}
         </Link>
-        <Link to="/TechWorld-TypeScript/buyersPage" className={`${styles.link} ${styles[type]}`}>
+        <Link to="/buyersPage" className={`${styles.link} ${styles[type]}`}>
           {t("hotBar.buyers")}
         </Link>
-        <Link to="/TechWorld-TypeScript/blogPage" className={`${styles.link} ${styles[type]}`}>
+        <Link to="/blogPage" className={`${styles.link} ${styles[type]}`}>
           {t("hotBar.blog")}
         </Link>
-        <Link to="/TechWorld-TypeScript/contactsPage" className={`${styles.link} ${styles[type]}`}>
+        <Link to="/contactsPage" className={`${styles.link} ${styles[type]}`}>
+          {t("hotBar.contacts")}
+        </Link>
+        <Link to="/admin" className={`${styles.link} ${styles[type]}`}>
           {t("hotBar.contacts")}
         </Link>
       </div>
